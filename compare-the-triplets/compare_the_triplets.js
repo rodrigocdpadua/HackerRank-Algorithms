e5fd5d342a6f8977061039d6297dacbd82cbf8a5
Rodrigo Campos <60583119+rodrigocdpadua@users.noreply.github.com>
2021-09-16 14:31:59 -0300

    update var -> let

--- compare-the-triplets/compare_the_triplets.js
+++ compare-the-triplets/compare_the_triplets.js
@@ -1,12 +1,12 @@
 function compareTriplets(a, b) {
-  var arrScore = [0,0];
-  for(var i = 0; i < 3; i++){
+  let arrScore = [0,0];
+  for(let i = 0; i < 3; i++){
       a[i] > b[i] ? arrScore[0]++ : a[i] < b[i] ? arrScore[1]++ : '';
   }
   return arrScore;
 }
 
-arr1 = [1, 2, 3];
-arr2 = [2, 1, 5];
+let arr1 = [1, 2, 3];
+let arr2 = [2, 1, 5];
 
 console.log(compareTriplets(arr1, arr2));
